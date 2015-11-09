ad7850f3360c525d37ded633186b6f651259dc2e
oscho707 <oscho707@student.liu.se>
2015-11-09 14:52:53 +0100

    Lagt till variabler för testning och openshift deployment, samt fixat dependencies

--- api.js
+++ api.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
-var url = 'mongodb://localhost:27017/plask';
+var db_name = "plask";
+
+mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + db_name;
+if(process.env.OPENSHIFT_MONGODB_DB_URL){
+  mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
+}
+
+var url = mongodb_connection_string; //more readable
 
 module.exports = (function() {
     'use strict';
@@ -16,6 +23,16 @@ module.exports = (function() {
             api.get("/getposts", function(req, res) {
                 db.collection("posts", function(err, coll) {
                     coll.find({}, function(err, cursor) {
+                        cursor.toArray(function(err, docs) {
+                            res.send(docs);
+                        });
+                    });
+                });
+            });
+
+            api.get("/:search", function(req, res) {
+                db.collection("posts", function(err, coll) {
+                    coll.find({text: {$regex: req.params.search}}, function(err, cursor) {
                         cursor.toArray(function(err, docs) {
                             res.send(docs);
                         });
--- server.js
+++ server.js
@@ -4,9 +4,13 @@ var api = require('./api');
 
 app.use('/api', api);
 
-var server = app.listen(3000, function () {
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+var server = app.listen(server_port, server_ip_address, function () {
     var host = server.address().address;
     var port = server.address().port;
+    console.log( "Listening on " + server_ip_address + ", server_port " + port )
 });
 
 module.exports.server = server;
