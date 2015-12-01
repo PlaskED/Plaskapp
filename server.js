8bac0fe7ca76d02ca36db59490df0f9a4560a976
oscho707 <oscho707@student.liu,se>
2015-12-01 13:47:31 +0100

    fixat routes så bodyparse funkar.

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ var app = express();
 var api = require('./api');
 var bodyParser = require("body-parser");
 
-app.use('/api', api);
-//app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use('/api', api);
 
 var server_port = 3000;
 var server_ip_address = '128.199.43.215'; // '127.0.0.1'
