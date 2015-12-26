472307c20d20164397fd238e24b9a1f842fdbbe4
oscho707 <oscho707@student.liu,se>
2015-12-26 12:41:04 +0100

    ..

--- database.js
+++ database.js
@@ -2,13 +2,18 @@ var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
 var url = 'mongodb://localhost:27017/plask';
 
-var getAll = function(resCB) {
-    module.exports.posts.find({}, function(err, cursor){
-        cursor.toArray(function(err, docs) {
-	    for (var i=0 ; i < docs.length ; i++) {
-		delete docs[i].location
-	    }
-	    resCB(docs); 
+var getAll = function(lpid, resCB) {
+    module.exports.posts.find({ pid: { $lt: lpid } }, function(err, cursor){
+        cursor.limit(10).toArray(function(err, docs) {
+	    module.exports.posts.findOne({ $query: {}, $orderby: { pid : -1 } }, function(err,ndoc) {
+		if (docs.length < 10) {
+		    docs.unshift(ndoc);
+		}
+		for (var i=0 ; i < docs.length ; i++) {
+		    delete docs[i].location
+		}
+		resCB(docs);
+	    });
         });
     });
 };
